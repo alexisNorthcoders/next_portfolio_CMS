e9647a7e343055f2a43acbcafa32c9dd184b70d5
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-02 15:29:13 +0100

    add navigation items, install shadcn Navigation Menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,61 @@
+"use client";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const navigationItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About me",
+    href: "/about",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Contact",
+    href: "/gallery",
+  },
+];
 export function Navbar() {
+  const pathname = usePathname()
   return (
     <nav className="max-w-7xl mx-auto px-4 md:px-8 py-5 grid grid-cols-12">
       <div className="col-span-6 flex md:col-span-3">
         <Link href="/">
-          <h1 className="text-3xl font-semibold"><span className="text-yellow-500">Ling Ling </span>Clown</h1>
+          <h1 className="text-3xl font-semibold">
+            <span className="text-yellow-500">Ling Ling </span>Clown
+          </h1>
         </Link>
+      </div>
+      <div className="hidden sm:flex justify-center items-center col-span-6">
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navigationItems.map((item, index) => {
+              return (
+                <NavigationMenuItem key={index}>
+                  <Link href={item.href} legacyBehavior passHref>
+                    <NavigationMenuLink active={pathname === item.href}
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      {item.name}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              );
+            })}
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </nav>
   );
