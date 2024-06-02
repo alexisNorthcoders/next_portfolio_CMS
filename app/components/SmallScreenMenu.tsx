b46afd0e1389758d7a801ebcf270471b89edf1c7
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-02 15:58:12 +0100

    add styling and close button to the small menu

--- app/components/SmallScreenMenu.tsx
+++ app/components/SmallScreenMenu.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import { navigationItems } from "./Navbar";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
 
 export function SmallScreenMenu(){
+    const location = usePathname()
     return (
         <Sheet>
             <SheetTrigger asChild>
@@ -16,12 +20,17 @@ export function SmallScreenMenu(){
                 <div className="mt-5 flex px-2 space-y-1 flex-col">
                     {navigationItems.map((item,index)=>{
                         return (
-                            <Link key={index} href={item.href}>
+                            <Link key={index} href={item.href} className={cn(location === item.href ? "bg-muted": " hover:bg-blue-700 hover:bg-opacity-50 ","group flex items-center px-2 py-2 text-md font-bold rounded-md")}>
                                     {item.name}
                             </Link>
                         )
                     })}
                 </div>
+                <SheetFooter className="mt-5">
+                    <SheetClose asChild>
+                        <Button type="submit">Close</Button>
+                    </SheetClose>
+                </SheetFooter>
             </SheetContent>
         </Sheet>
     )
