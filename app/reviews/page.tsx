5aad79699d51126bba504a6976f062a77aed9125
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-03 15:04:16 +0100

    add query to fetch reviews from database

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,20 +1,39 @@
-import { Card,CardHeader } from "@/components/ui/card";
+import { Card, CardHeader } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import ReviewForm from "../components/ReviewForm";
+import prisma from "../lib/db";
 
-export default function ReviewPage(){
-    return(
-        <section className="max-w-7xl w-full px-4 md:px-8 mx-auto">
+async function getReviews() {
+  const review = await prisma.reviews.findMany({
+    select: {
+      User: {
+        select: {
+          firstName: true,
+          profileImage: true,
+        },
+      },
+      message: true,
+      id: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 30,
+  });
+}
 
-            <h1 className="text-4xl font-semibold lg:text-5xl pt-5">Reviews</h1>
-            <p className="leading-7 text-muted-foreground mt-2">Leave a Review!</p>
+export default function ReviewPage() {
+  return (
+    <section className="mx-auto w-full max-w-7xl px-4 md:px-8">
+      <h1 className="pt-5 text-4xl font-semibold lg:text-5xl">Reviews</h1>
+      <p className="mt-2 leading-7 text-muted-foreground">Leave a Review!</p>
 
-            <Card className="mt-10">
-                <CardHeader className="flex flex-col w-full">
-                    <Label>Message</Label>
-                    <ReviewForm/>
-                </CardHeader>
-            </Card>
-        </section>
-    )
+      <Card className="mt-10">
+        <CardHeader className="flex w-full flex-col">
+          <Label>Message</Label>
+          <ReviewForm />
+        </CardHeader>
+      </Card>
+    </section>
+  );
 }
