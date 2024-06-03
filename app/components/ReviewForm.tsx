a094052973f0d8254e8f83d054b958d77244f154
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-03 11:35:23 +0100

    add authentication route

--- app/components/ReviewForm.tsx
+++ app/components/ReviewForm.tsx
@@ -1,20 +1,17 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-export default function ReviewForm(){
-    const user = ""
+export default function ReviewForm() {
+  const user = "";
 
-    if (user){
-        return (
-            <div>This is my review!</div>
-        )
-    }
-    else{
-        return (
-            <div className="flex justify-between gap-4 flex-col md:flex-row">
-                <Input type="text" placeholder="Your review here..."/>
-                <Button>Submit Review</Button>
-            </div>
-        )
-    }
+  if (user) {
+    return <div>This is my review!</div>;
+  } else {
+    return (
+      <div className="flex flex-col justify-between gap-4 md:flex-row">
+        <Input type="text" placeholder="Your review here..." />
+        <Button>Submit Review</Button>
+      </div>
+    );
+  }
 }
