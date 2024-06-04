9b05b49e35c7d976ecdc3e22d7bac64b34f68b15
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-04 18:18:26 +0100

    add ProjectsCard interface

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,6 @@
+import { ProjectsCard } from "../lib/interface";
+import { client } from "../lib/sanity";
+
 async function getProjects(){
   const query =`*[_type == "project"]{
     title,
@@ -7,9 +10,13 @@ async function getProjects(){
           tags,
       "imageUrl":image.asset -> url
   }`
+  const data = await client.fetch(query)
+  return data
 }
 
-export default function Projects() {
+
+export default async function Projects() {
+  const data: ProjectsCard = await getProjects()
   return <section className="mx-auto w-full max-w-7xl px-4 md:px-8">
     <h1 className="text-4xl font-semibold lg:text-5xl">Projects</h1>
     <p className="leading-7 text-muted-foreground mt-2">Check out my projects </p>
