b76defb4c093025d2d27ab61a9cf0c2e9b258e3b
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-06 16:45:44 +0100

    change name

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
       <div className="col-span-1 h-full min-h-[500px] rounded-2xl bg-gray-100 p-8 md:col-span-2 lg:min-h-[300px]">
-        <h1 className="text-4xl font-medium md:text-6xl">Hi! I'm Alexis </h1>
+        <h1 className="text-4xl font-medium md:text-6xl">Hi! I'm<span className="text-primary"> Random</span></h1>
         <h2 className="mb-3 mt-3 text-xl font-normal md:text-3xl lg:text-6xl">
           👋 I'm a software developer on a mission to create tech magic ✨. From
           crafting sleek interfaces to weaving complex algorithms, I'm here to
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -40,7 +40,7 @@ export function Navbar() {
       <div className="flex">
         <Link href="/">
           <h1 className="text-3xl font-semibold">
-            Alexis<span className="text-primary"> Creoulo</span>
+            John<span className="text-primary"> Random</span>
           </h1>
         </Link>
       </div>
