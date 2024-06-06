ea9eaa72864b290a570580e09aa8054f5e579cfd
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-06-06 17:02:09 +0100

    add method to not cache api request

--- app/api/auth/create/route.ts
+++ app/api/auth/create/route.ts
@@ -1,8 +1,10 @@
 import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextResponse } from "next/server";
+import {unstable_noStore} from "next/cache"
 
 export async function GET() {
+  unstable_noStore()
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
